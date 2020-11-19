5dd6c1287cacf8c6adbe71a6f55b6c786cba1b12
JamieBamber <jarb2@alumni.cam.ac.uk>
2020-11-19 17:02:30 +0000

    Create HeunC.py

---
+++ HeunC.py
@@ -0,0 +1,5 @@
+import numpy as np
+import ctypes
+
+HeunClib = ctypes.cdll.LoadLibrary('HeunC_lib.so')
+Kerrlib.Rfunc.argtypes = [ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_int, ctypes.c_int
